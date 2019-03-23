d0525eebd22d8559560102758d7476ea6570b878
ash <ashrantkohli@stockx.com>
2019-03-23 18:19:45 -0400

    first thing
    the first one of itoos many things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import FlippyCard from './FlippyCard'
+
+const ButtonsContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%)
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ColourButton = styled.button`
+  text-align: center;
+  position: relative;
+  background: rgba(${props => props.buttonColorR}, ${props => props.buttonColorG}, ${props => props.buttonColorB}, ${props => props.editOpacity * 0.075 + 0.25});
+  width: 100%;
+  z-index:-1;
+  padding: 30px 30px;
+  &::before{
+    content: "";
+    display: block;
+    padding-top: 100%;
+  }
+`;
+
+const Content = styled.div`
+  font-size: 16px;
+  opacity: 1;
+	position:  absolute;
+	top: 45%;
+	left: 0;
+	bottom: 0;
+  right: 0;
+`;
+
+const MiddleButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 200%;
+`;
 
 class App extends Component {
+  state = {
+    redOpacity: 0,
+    greenOpacity: 0,
+    yellowOpacity: 0,
+    blueOpacity: 0,
+  }
+
+  onClickRed() 
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ButtonsContainer>
+          <ButtonContainer>
+            <ColourButton buttonColorR={255} buttonColorG={0} buttonColorB={0} editOpacity={this.state.redOpacity}>
+              <Content>TEXT #1</Content>
+            </ColourButton>
+            <MiddleButtons>
+              <ColourButton buttonColorR={0} buttonColorG={255} buttonColorB={0} editOpacity={this.state.greenOpacity}>
+              <Content>TEXT #2</Content>
+              </ColourButton>
+              <ColourButton buttonColorR={255} buttonColorG={255} buttonColorB={0} editOpacity={this.state.yellowOpacity}>
+              <Content>TEXT #3</Content>
+              </ColourButton>
+            </MiddleButtons>
+            <ColourButton buttonColorR={0} buttonColorG={0} buttonColorB={255} editOpacity={this.state.blueOpacity}>
+              <Content>TEXT #4</Content>
+            </ColourButton>
+          </ButtonContainer>
+        </ButtonsContainer>
+        
       </div>
     );
   }
